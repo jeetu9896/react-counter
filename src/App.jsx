5ce795164557848d3cc713c54e93afe1f81fc42d
jeetu9896 <301996jit@gmail.com>
2025-10-07 16:54:39 +0530

    add counter using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import CounterNew from "./components/CounterNew";
 
 function App() {
   return (
     <>
       <h1> React counter app</h1>
-      <Counter />
+      <div className="flex justify-center mt-20 w-full align-center">
+        <Counter />
+        <CounterNew />
+      </div>
     </>
   );
 }
