b5cc787ca2f0e91921094e138c1ea49efec674d9
jeetu9896 <301996jit@gmail.com>
2025-11-05 17:36:30 +0530

    Upgrade package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,13 +15,14 @@ export default defineConfig({
     tailwindcss(),
   ],
   test: {
-    include: ['src/**/*.test.{js,jsx,ts,tsx}'],
-    environment: 'jsdom',
+    include: ["src/**/*.test.{js,jsx,ts,tsx}"],
+    environment: "jsdom",
     globals: true,
-    setupFiles: './src/setupTests.js',
+    setupFiles: "./src/setupTests.js",
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      provider: "v8",
+      reporter: ["text", "json", "html"],
     },
+    reporters: ["default", "hanging-process"],
   },
 });
